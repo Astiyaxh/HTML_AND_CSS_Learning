b5c8a8765c565488e6530c90b1cd92e9aeedf6c3
Astiyaxh <ariyan.mehri00@gmail.com>
2023-03-18 17:10:13 +0330

    part two is complete

---
+++ Part2/js/script46.js
@@ -0,0 +1,10 @@
+var box = document.getElementsByClassName("box")[0];
+box.addEventListener('onclick', function(event) {
+    box.classList.add("animation-pop");
+})
+
+
+function pop(box) {
+    box.ClassList.add("animation-pop");
+    console.log("fuck")
+}
